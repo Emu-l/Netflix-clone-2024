76783f0991a8a5ecdb9a6fc51b2d416422496b11
kemaryam <146838128+kemaryam@users.noreply.github.com>
2024-01-30 23:26:31 -0800

    axios and request

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   );
 }
 
